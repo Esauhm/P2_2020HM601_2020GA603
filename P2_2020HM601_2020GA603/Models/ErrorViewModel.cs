b603bb54f4153b9425685caa013c943198da5a6f
DiegoHdez <88221088+Esauhm@users.noreply.github.com>
2023-04-29 12:06:21 -0600

    Agregar archivos de proyecto.

---
+++ P2_2020HM601_2020GA603/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2_2020HM601_2020GA603.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
