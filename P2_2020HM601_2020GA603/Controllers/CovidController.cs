913762529a2205575368e471a96bf96388aa2e2d
DiegoHdez <88221088+Esauhm@users.noreply.github.com>
2023-04-29 12:47:34 -0600

    Diego: Vista creada

---
+++ P2_2020HM601_2020GA603/Controllers/CovidController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P2_2020HM601_2020GA603.Controllers
+{
+    public class CovidController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
