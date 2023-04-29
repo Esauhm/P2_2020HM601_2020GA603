7953dc6e88bb84323c8136b213edfcc38b0bc8bb
DiegoHdez <88221088+Esauhm@users.noreply.github.com>
2023-04-29 13:17:01 -0600

    Diego: Vista casi terminada

--- P2_2020HM601_2020GA603/Controllers/CovidController.cs
+++ P2_2020HM601_2020GA603/Controllers/CovidController.cs
@@ -1,11 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using P2_2020HM601_2020GA603.Models;
 
 namespace P2_2020HM601_2020GA603.Controllers
 {
     public class CovidController : Controller
     {
+        private readonly DBCovidContext _CovidDbContext;
+
+        public CovidController(DBCovidContext covidDbContext)
+        {
+            _CovidDbContext = covidDbContext;
+        }
+
         public IActionResult Index()
         {
+            //Lista de departamentos
+            var listaDeDepartamentos = (from m in _CovidDbContext.departamentos
+                                 select m).ToList();
+
+            ViewData["listaDeDepartamentos"] = new SelectList(listaDeDepartamentos, "id_departamento", "nombre");
+
+
+            //lista de generos
+            //var listaDeGeneros = (from m in _CovidDbContext.generos
+            //                            select m).ToList();
+
+            //ViewData["listaDeGeneros"] = new SelectList(listaDeGeneros, "id_genero", "genero");
+
+
             return View();
         }
     }
